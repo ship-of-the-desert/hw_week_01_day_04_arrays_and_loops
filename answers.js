48e89588a6b73160f37e2ef7c5f01cf1200d4c56
sarahAbuhaimed <sar-abu@hotmail.com>
2019-02-27 16:38:49 +0300

    Get Even answered

--- answers.js
+++ answers.js
@@ -1,6 +1,11 @@
-//easy going
+//easy going exercise 
 var number = [0,1,2,3,4,5,6,7,8,9,10,];
 
     for (var i = 0; i < number.length; i++) {
         console.log(number[i]);
     }
+    //Get Even between 0 to 200
+    var numberEven = [];
+for (numberEven = 0;numberEven <= 200; numberEven++ ) {
+    if (numberEven% 2 == 0){
+        console.log(' the even number is: ' + numberEven);}}
