172d15a632b54041671e3920ccd1689ca10f7acf
Muhammad-Hakami <engmuhammadhakami@gmail.com>
2019-02-27 15:25:21 +0300

    Yell at Ninja Turtles

--- answers.js
+++ answers.js
@@ -3,10 +3,12 @@ for (var i=1;i<21; i++) {
     console.log(i)
 }
 
+
 // Get Even
 for (var i=0; i<21; i++) {
     (i%2===0)?console.log(i):0;
 }
+
 
 // Wild Wild Life
 const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
@@ -20,3 +22,10 @@ wolfy[3]='Gotham City'
 dart[3]+=', Hawkins'
 wolfy[0]='Gameboy'
 
+
+// Yell at Ninja Turtles
+var ninjaTurtles=['Donatello, Leonardo, Raphael, Michaelangelo'];
+
+for (let n of ninjaTurtles) {
+    console.log(n.toUpperCase())
+}
