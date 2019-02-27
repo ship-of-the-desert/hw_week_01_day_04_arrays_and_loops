2c4ae63dde36870ed8ce4d7077690174b000e625
Muhammad-Hakami <engmuhammadhakami@gmail.com>
2019-02-27 14:59:07 +0300

    Wild Wild Life solved

--- answers.js
+++ answers.js
@@ -1,3 +1,22 @@
+// Easy Going
 for (var i=1;i<21; i++) {
     console.log(i)
 }
+
+// Get Even
+for (var i=0; i<21; i++) {
+    (i%2===0)?console.log(i):0;
+}
+
+// Wild Wild Life
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee[2]+=1
+wolfy[3]='Gotham City'
+dart[3]+=', Hawkins'
+wolfy[0]='Gameboy'
+
