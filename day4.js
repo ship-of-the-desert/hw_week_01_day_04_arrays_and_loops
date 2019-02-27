403c2c4985fa65315783ddfdafca94c0da7e19f6
AceelAlArifi <aceelalarifi@gmail.com>
2019-02-27 16:42:35 +0300

    Get Even answered

--- day4.js
+++ day4.js
@@ -1,6 +1,11 @@
 // Easy Going:
 
-for (let index = 0; index <= 20; index++) {
+for (let index = 1; index <= 20; index++) {
     console.log(index);
 }
 
+// GEt Even
+
+for (let index = 0; index <= 200; index += 2) {
+    console.log(index);
+}
