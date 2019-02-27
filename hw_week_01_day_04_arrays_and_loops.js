88ef968f28df00c2047fa47b9460d6cb66d27a80
Mortaja Alqassab <mort8k@hotmail.com>
2019-02-27 14:37:28 +0300

    Wild Wild Life Answered

--- hw_week_01_day_04_arrays_and_loops.js
+++ hw_week_01_day_04_arrays_and_loops.js
@@ -43,3 +43,29 @@ for (var i = 1; i < 101; i++)
     }
     console.log(i + '   ' + result);
 }
+
+
+//*SECTION#4 WILD WILD LIFE
+//?commit message : "Wild Wild Life Answered"
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+const sharky = ["Sharky", "shark", 20, "Left Coast"];
+const plantee = ["Plantee", "plant",  5000 , "Mordor"];
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"];
+
+// Step#1; Change Plantee age
+plantee[2] += 1;
+// Step#2: Change wolfy hometome
+wolfy[3] = 'Gotham City';
+// Step#3: Give dart a second hometown 'Hawkins'
+dart.push('Hawkins');
+// Step#4: Remove walfy and add Gameboy
+wolfy.splice(1,1);
+wolfy.unshift('Gameboy');
+
+// Print arrays
+console.log(wolfy);
+console.log(sharky);
+console.log(plantee);
+console.log(porgee);
+console.log(dart);
