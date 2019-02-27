d8b713f877d32030da62365738624053ad6a6ba6
khuzamas <khuzamshu@gmail.com>
2019-02-27 20:00:14 +0300

    Wild Wild Life answered

--- answers.js
+++ answers.js
@@ -17,3 +17,33 @@ for (var i = 0; i <=200; i+=2) {
     console.log(i);
 }
 console.log('\n');
+
+//wild wild life
+console.log('Wild Wild Life-------------');
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+const sharky = ["Sharky", "shark", 20, "Left Coast"];
+const plantee = ["Plantee", "plant",  5000 , "Mordor"];
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"];
+
+//1. changing plantee's age
+plantee[2]= plantee[2] + 1;
+console.log(plantee);
+
+//2. changing wolfy's hometown
+wolfy[3]= 'Gotham City';
+console.log(wolfy);
+
+//3. giving dart 2nd hometown
+dart.push('Hawkins');
+console.log(dart);
+
+//4. renaming wolfy
+wolfy.shift();
+wolfy.unshift('Gameboy');
+console.log(wolfy);
+console.log('\n');
+
+
+
+
