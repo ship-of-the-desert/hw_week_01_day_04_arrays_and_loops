4031ccd76b21b4abaa4cefa0d4a5831a1dd37f0d
Yasir S <yasser9946.1@gmail.com>
2019-02-28 01:07:58 +0300

    Find the Median answered

---
+++ Find_the_Median.js
@@ -0,0 +1,9 @@
+const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+nums.sort();
+//console.log(nums);// [ 11,11,11,11,12,12,12,12,13,14,14,15,15,15,15,16,16,17,17,17,18,18,19,19,71 ]
+//console.log(nums.length); // 25
+//If the number of these values ​​is odd, the median is the half digit that divides these values,
+//If the number of values ​​is equal, the median is the arithmetic mean of the sum of the two median numbers.
+console.log(nums[13-1]);// be cus start from 0 
+
+// ohh fanaly i can sleep 
