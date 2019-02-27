8340d33dfb8a02d468a912dda646eb6d0ff14f28
amani11 <amani.g.66@gmail.com>
2019-02-27 23:49:53 +0300

    Find the Median answered

--- answer.js
+++ answer.js
@@ -36,4 +36,43 @@ for(let ninja of ninjaTurtles){
   console.log(`${ninja.toLocaleUpperCase()}`);
 }
 
+//Methods, Revisited
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+console.log(favMovies.indexOf("Titanic"));
+favMovies.sort();
+console.log(favMovies);
+favMovies.pop();
+favMovies.push("Guardians of the Galaxy");
+favMovies.reverse();
+favMovies.shift();
+favMovies.unshift("beautiful mind");
+favMovies.splice(favMovies.indexOf("Django Unchained"), 1,"Avatar");
+console.log(favMovies);
+var newF=favMovies.slice(favMovies.length/2);
+console.log(` new Faf ${newF}`);
+console.log(favMovies.indexOf("Fast and Furious"));
 
+
+//Where is Waldo
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", ["Baked Goods", "Waldo"]]];
+                    console.log(whereIsWaldo);
+                    console.log(whereIsWaldo.indexOf("Eggbert"));
+                    whereIsWaldo.splice(whereIsWaldo[1],1);
+                    console.log(whereIsWaldo);
+                   console.log(whereIsWaldo.indexOf("Waldo"));//[3][1][1]);
+                  
+
+//Excited Kitten
+var kittenTalkingPoints=["...human...why you taking pictures of me?...", "...the catnip made me do it...","...why does the red dot always get away..."];
+var random=Math.random();
+for (i=0;i<=20;i++){
+  console.log("Love me, pet me! HSSSSSS!");
+
+}                   
+//Find the Median
+const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+nums.sort();
+console.log(nums);
+console.log(nums[Math.floor((nums.length)/2)]);
