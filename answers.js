6e074dede73fa5e16bbbd113a806128d5aacb44b
khuzamas <khuzamshu@gmail.com>
2019-02-27 16:46:59 +0300

    Get Even answered

--- answers.js
+++ answers.js
@@ -9,3 +9,11 @@ for (var i = 1; i <= 20; i++) {
 }
 console.log('\n');
 
+//Get even
+/* for loop prints even #s (0-200)
+*/
+console.log('Get even-------------');
+for (var i = 0; i <=200; i+=2) {
+    console.log(i);
+}
+console.log('\n');
