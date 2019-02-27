e8bd6f6f2690e8c7f819b982f29f39a66780a2a6
amani11 <amani.g.66@gmail.com>
2019-02-27 21:03:03 +0300

    Wild Wild Life answered

--- answer.js
+++ answer.js
@@ -2,7 +2,7 @@
 for (i=0;i<=20;i++){
   console.log(i);
 }
-
+ //......................
 // Get Even
 for (i=0;i<=200;i++){
   if (i%2==0){
@@ -10,4 +10,23 @@ for (i=0;i<=200;i++){
     }
     }
     //......................
-    
+
+  // Wild Wild Life  
+  const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+  const sharky = ["Sharky", "shark", 20, "Left Coast"]
+  const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+  const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+  const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+  plantee[2]=5001;
+  wolfy[3]="Gotham City";
+  dart[3]=dart[3]+" and Gotham City";
+  wolfy.shift();
+  wolfy.unshift("Gameboy");
+  
+  console.log(wolfy);
+  console.log(sharky);
+  console.log(plantee);
+  console.log(porgee);
+  console.log(dart);
+
