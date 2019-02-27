a5bb93bdea8e9a83e4824b4b5bdf2d9365944884
Mortaja Alqassab <mort8k@hotmail.com>
2019-02-27 14:18:28 +0300

    Easy Going Answered

---
+++ hw_week_01_day_04_arrays_and_loops.js
@@ -0,0 +1,16 @@
+/*
+ * Filename: c:\Users\mort8k\Documents\Courses\wdi4\ship_of_the_desert\week01\hw_week_01_day_04_arrays_and_loops\hw_week_01_day_04_arrays_and_loops.js
+ * Path: c:\Users\mort8k\Documents\Courses\wdi4\ship_of_the_desert\week01\hw_week_01_day_04_arrays_and_loops
+ * Created Date: Wednesday, February 27th 2019, 2:11:18 pm
+ * Author: Mortaja Alqassab <moqassa@gmail.com>
+ * 
+ * Copyright (c) 2019 MiSK Academy
+ */
+
+ 
+//*SECTION#1 EASY GOING
+//?commit messge : "Easy Going Answered"
+for (var i = 1; i < 21; i++)
+{
+    console.log(i);
+}
