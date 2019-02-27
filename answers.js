62d9e037443d1bd035f50ce37952a14076edb53b
khuzamas <khuzamshu@gmail.com>
2019-02-27 16:43:35 +0300

    Easy Going answered

---
+++ answers.js
@@ -0,0 +1,11 @@
+//Name: Khuzam Shubbar
+
+//Easy going
+/* for loop prints 1 through 20
+*/
+console.log('Easy Going-------------');
+for (var i = 1; i <= 20; i++) {
+    console.log(i);
+}
+console.log('\n');
+
