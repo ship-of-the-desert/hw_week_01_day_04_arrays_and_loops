09c963577f8799e4b691222e27f41065308ddbc7
mohammed <alhammad2013@gmail.com>
2019-02-28 09:12:16 +0300

    homework

---
+++ answers.js
@@ -0,0 +1,117 @@
+// Easy Going
+var count = 1;
+
+while (count < 20 ){
+    console.log(count+' Easy Going answered');
+    count=count+1;
+};
+
+
+//////////////////////
+// Get Even
+
+for (var i=0; i <= 200; i++){
+    if ((i%2)==0){
+        console.log(i+' Get Even answered')
+}
+}
+
+//////////////////////
+// Wild Wild Life
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+
+plantee[2]=5001;
+wolfy[3]='Gotham City';
+dart[3]=
+
+
+////////////////////
+// Yell at the Ninja Turtles
+
+
+ turtles = [ "Donatello", "Leonardo", "Raphael", "Michaelangelo" ];
+
+// for of (toUpperCase(turtles)){
+//     console.log(turtles)
+// }
+
+
+
+
+
+
+
+
+//////////////////////
+// favorite movies
+
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle',
+ 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects',
+  'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 
+  'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 
+  'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+
+
+
+
+
+
+
+
+
+  //////////////////////
+  // Where is Waldo
+
+  const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", ["Baked Goods", "Waldo"]]];
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////
+// Excited Kitten
+
+var count = 0;
+
+while (count <= 20 ){
+    console.log(' Love me, pet me! HSSSSSS!');
+    count=count+1;
+};
+
+const kittyTalk = ["...human...why you taking pictures of me?...", "...the catnip made me do it...",
+"...why does the red dot always get away..."];
+
+
+kittyTalk[0];
+kittyTalk[1];
+kittyTalk[2];
+
+let meow = 0; 
+kittyTalk[meow]
+
+
+//////////////////
+// Find the Median
+
+var nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+
+median(nums);
+nums.sort(function(a, b) {return a - b; });
+middle(nums);
+
+// median(nums)
+// console.log(nums);
