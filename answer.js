eae39ac1af970cfb5b3e9b99b321d7127bbc8e88
amani11 <amani.g.66@gmail.com>
2019-02-28 07:18:18 +0300

    Methods, Revisited answered

--- answer.js
+++ answer.js
@@ -38,17 +38,17 @@ for(let ninja of ninjaTurtles){
 const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
 console.log(favMovies.indexOf("Titanic"));
 favMovies.sort();
-console.log(favMovies);
+//console.log(favMovies);
 favMovies.pop();
 favMovies.push("Guardians of the Galaxy");
 favMovies.reverse();
 favMovies.shift();
 favMovies.unshift("beautiful mind");
 favMovies.splice(favMovies.indexOf("Django Unchained"), 1,"Avatar");
-console.log(favMovies);
-var newF=favMovies.slice(favMovies.length/2);
-console.log(` new Faf ${newF}`);
-console.log(favMovies.indexOf("Fast and Furious"));
+//console.log(favMovies);
+var newfavMovies=favMovies.slice(favMovies.Math.floor(length/2));
+console.log(` new Faf ${newfavMovies}`);
+console.log(newfavMovies.indexOf("Fast and Furious"));
 
 
 //Where is Waldo
