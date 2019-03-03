407da17fc95e52f81565437d07c1577814960e11
rehabsaadoun <YOUR GITHUB EMAIL>
2019-03-03 17:28:54 +0300

    hq d04w01

---
+++ answrs.js
@@ -0,0 +1,73 @@
+
+1//////
+
+for (var i=1; i <=20; i++);{
+console.log("Easy going answered");
+}
+2//////
+
+for (var i =0 ; i <=200; i++)
+   { if (i % 2 == 0)
+    console.log("i");
+}
+3////////////
+ 
+var  Plantee,wolfy,dart,
+for (var i = 1; i<=5000; i++);{
+console.log( Plantee);
+}
+var  Plantee;
+for ( var index = 1; index <= 5000; index += 5000 );
+{
+    var Plantee = [index];
+    console.log( Plantee );
+  }
+
+or
+plantee.splice("brithday",1<+500);
+woly.splice(3,0,"Gothom city");
+dart.splice(1,0,"homtwon");
+wolfy.splice(0,1"Gameboy");
+
+
+4////////////////////////////////// 
+
+var ninjaTurtles = [
+    "Donatello",
+    "Leonardot",
+    "Raphael",
+    "Michaelangelo"
+  ];
+  console.log("Yell at the Ninja Turtles answered");
+
+  console.log(forMovies,idexOf"Titanic");
+  forMovies.();
+  forMovies.sorot  //(the array depend an alphapet order)
+  forMovies.pop();
+  forMovies.push("frineds");
+  forMovies.(reversel);
+  forMovies.shift();
+  forMovies.unshift("spiderman");
+  forMovies.splice(4.0,"Advter");
+  forMovies.splice(10);
+  console.log(forMovies,sor);
+  sort.Array //depend on alphapet
+//////
+for(var count=0;count <nums,length, count++);
+var (size = nums.length);
+if(size %2 == 0){
+var center = math,floor(centrr)+1};
+median = nums{center}
+console.log(Medim);
+else if (size % ==0)
+{var center 1 = size 2};
+var centet 2 = ((center %2)+1);
+median =(((center1 +center2 )%2);
+console.log(median);
+
+
+
+
+
+  
+
